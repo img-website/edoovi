5fe1c591a0f616058f1e0be736835cf8d45093e8
shivtejimg <shivtej.img@gmail.com>
2024-08-02 10:59:35 +0530

    jk

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>edoovi</title>
+  <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+  <h1 class="text-4xl text-red-300">hello</h1>
+</body>
+</html>
