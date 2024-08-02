c20d6dee3e0dcaa75630882ccd0650325ea8c4ec
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2024-08-02 11:34:25 +0530

    themeColor

---
+++ shubham/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>edoovi</title>
+  <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+  <h1 class="text-3xl text-red-700 py-5">hello</h1>
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./**/*.{php,js}"],
   theme: {
     extend: {
-      
+      colors: {
+        'theme1': '#e67529',
+        'theme2': '#b09ffe',
+        
+      },
     },
   },
   plugins: [],
