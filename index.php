b7668bb8c0efdd87239d7acd4cee59dae8dc416d
shivtejimg <shivtej.img@gmail.com>
2024-08-02 15:47:16 +0530

    heroSection

--- index.php
+++ index.php
@@ -5,8 +5,54 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>edoovi</title>
   <link rel="stylesheet" href="./assets/css/style.css">
+  <!-- swiper link  -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
 </head>
 <body>
-  <h1 class="text-3xl text-red-700 py-5">hello</h1>
+  <!-- hero section  -->
+  <section class="relative h-dvh">
+    <div class="mx-auto max-w-7xl lg:px-8 px-6 h-full">
+      <div class="flex items-center flex-wrap h-full">
+        <div class="w-1/2 pe-5">
+          <div class="text-slate-900 text-7xl font-semibold"> Investing in  knowledge and <span class="text-orange-500 text-7xl font-semibold">Your Future</span></div>
+          <div class="text-gray-500 text-sm mt-3 font-medium leading-normal pe-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident animi veniam cupiditate et nobis, omnis similique distinctio iste maxime enim quo. Quod autem dolorem dolores?</div>
+          <div class="grid grid-cols-3 items-center mt-5">
+            <div>
+            <button class="bg-gradient-to-b from-violet-300 to-violet-500/90 py-2 px-7 rounded-full shadow-xl shadow-violet-200/95 font-medium text-base text-white">Contact </button>
+            </div>
+            <div>
+              <div class="text-slate-900 text-4xl font-semibold">50<span class="text-3xl">+</span></div>
+              <div class="text-gray-500 text-sm ">Career Courses</div>
+            </div>
+            <div>
+              <div class="text-slate-900 text-4xl font-semibold">1M<span class="text-3xl">+</span></div>
+              <div class="text-gray-500 text-sm ">Our  Students</div>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/2  flex flex-col items-center justify-center aspect-square shrink-0 relative">
+        <div class='w-[600px] aspect-square rounded-full bg-[#F6F4FC] md:p-10 p-4'>
+            <div class='w-full aspect-square rounded-full bg-[#EFECFE] md:p-10 p-4 '>
+                <div class='w-full aspect-square rounded-full bg-[#DCD6FE] md:p-10 p-4 '>
+                    <div class='w-full relative aspect-square rounded-full bg-[#9985FC] '>
+                        <div class='w-full absolute inset-x-0 bottom-0 aspect-[9/13] px-2 rounded-full flex items-end'>
+                            <Image class='rounded-full size-full object-cover object-bottom' src="./assets/img/heroImg.png" alt='sdf' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>'
+          
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- hero section end  -->
 </body>
+
+
+
 </html>
