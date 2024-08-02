cd365b511f133db0682c420dc9b1c64d1c37f9f6
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2024-08-02 17:21:34 +0530

    clients section

--- shubham/index.php
+++ shubham/index.php
@@ -1,12 +1,230 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>edoovi</title>
-  <link rel="stylesheet" href="./assets/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>edoovi</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
+
 <body>
-  <h1 class="text-3xl text-red-700 py-5">hello</h1>
+    <div class="">
+        <div class="overflow-hidden px-5 py-12">
+            <div class="relative container mx-auto max-w-7xl md:px-6 lg:px-8 z-10">
+                <p class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-black">
+                    <span class="font-normal">Our Esteemed </span>
+                    <span class="font-bold text-theme1">Clients</span>
+                </p>
+                <div class="md:text-base text-sm text-black/70 mt-2 font-bold">
+                    Embark on an Automation Journey with Us
+                </div>
+            </div>
+            <div class="mt-8">
+                <div class="swiper mySwiper2 swiper-initialized swiper-horizontal swiper-backface-hidden">
+                    <div class="swiper-wrapper !ease-linear" id="swiper-wrapper-dd31f7415a72d890" aria-live="off" style="transition-duration: 2000ms; transform: translate3d(-382.4px, 0px, 0px);">
+                        <div class="swiper-slide" role="group" aria-label="7 / 7" data-swiper-slide-index="6" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper03.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-prev" role="group" aria-label="1 / 7" data-swiper-slide-index="0" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper01.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-active" role="group" aria-label="2 / 7" data-swiper-slide-index="1" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper05.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-next" role="group" aria-label="3 / 7" data-swiper-slide-index="2" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper03.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide" role="group" aria-label="4 / 7" data-swiper-slide-index="3" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper04.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide" role="group" aria-label="5 / 7" data-swiper-slide-index="4" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper05.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide" role="group" aria-label="6 / 7" data-swiper-slide-index="5" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper03.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+                </div>
+            </div>
+            <div class="mt-8">
+                <div class="swiper  mySwiper3 swiper-initialized swiper-horizontal swiper-backface-hidden" dir="rtl">
+                    <div class="swiper-wrapper !ease-linear" id="swiper-wrapper-dd31f7415a72d890" aria-live="off" style="transition-duration: 2000ms; transform: translate3d(-382.4px, 0px, 0px);">
+                        <div class="swiper-slide" role="group" aria-label="7 / 7" data-swiper-slide-index="6" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper0201.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-prev" role="group" aria-label="1 / 7" data-swiper-slide-index="0" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper0202.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-active" role="group" aria-label="2 / 7" data-swiper-slide-index="1" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper0203.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-next" role="group" aria-label="3 / 7" data-swiper-slide-index="2" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper0201.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide" role="group" aria-label="4 / 7" data-swiper-slide-index="3" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper0202.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide" role="group" aria-label="5 / 7" data-swiper-slide-index="4" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper0203.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide" role="group" aria-label="6 / 7" data-swiper-slide-index="5" style="width: 176.2px; margin-right: 15px;">
+                            <div class="size-full">
+                                <div class="img h-[83px] w-auto">
+                                    <img class="size-full object-contain" src="../assets/img/swiper0201.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script>
+        var swiper = new Swiper(".mySwiper2", {
+            loop: true,
+            speed: 4000,
+            slidesPerView: "auto",
+            centeredSlides: false,
+            allowTouchMove: false,
+            // slidesPerView: 3,
+            spaceBetween: 30,
+            autoplay: {
+                enabled: true,
+                delay: 0
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.8,
+                },
+                360: {
+                    slidesPerView: 1.8,
+                },
+                576: {
+                    slidesPerView: 2.8,
+                },
+                768: {
+                    slidesPerView: 5,
+                },
+                992: {
+                    slidesPerView: 6,
+                },
+                1200: {
+                    slidesPerView: 6,
+                },
+                1400: {
+                    slidesPerView: 6,
+                },
+            },
+        });
+    </script>
+    <script>
+        var swiper = new Swiper(".mySwiper3", {
+            loop: true,
+            speed: 4000,
+            slidesPerView: "auto",
+            centeredSlides: false,
+            allowTouchMove: false,
+            // slidesPerView: 3,
+            spaceBetween: 30,
+            autoplay: {
+                enabled: true,
+                delay: 0
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.8,
+                },
+                360: {
+                    slidesPerView: 1.8,
+                },
+                576: {
+                    slidesPerView: 2.8,
+                },
+                768: {
+                    slidesPerView: 5,
+                },
+                992: {
+                    slidesPerView: 6,
+                },
+                1200: {
+                    slidesPerView: 6,
+                },
+                1400: {
+                    slidesPerView: 6,
+                },
+            },
+        });
+    </script>
 </body>
+
 </html>
