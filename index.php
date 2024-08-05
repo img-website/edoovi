2927eb5fbd33af74710db1f2d443e889f2a00ab4
shivtejimg <shivtej.img@gmail.com>
2024-08-05 14:19:00 +0530

    jk

--- index.php
+++ index.php
@@ -816,6 +816,108 @@
     </div>
     <!-- testimonial section ends -->
 
+
+    <!-- blog section  -->
+    <section>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 w-full shadow-md">
+    <div class="grid grid-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-9">
+    <div class="col-auto group">
+        <div class="size-full p-5 rounded-2xl  [box-shadow:0px_0px_32px_0px_rgba(36,40,46,.12)]   flex flex-col justify-center items-center text-center
+         group-hover:bg-gradient-to-r from-[skyblue]/20 via-[skyblue]/50 to-[skyblue]/20 group-hover:border-transparent duration-300 transition-all 
+         group-hover:scale-95">
+          <div class="*:size-12 sm:*:size-14">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M12 1 3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm7 10c0 4.52-2.98 8.69-7 9.93-4.02-1.24-7-5.41-7-9.93V6.3l7-3.11 7 3.11V11zm-11.59.59L6 13l4 4 8-8-1.41-1.42L10 14.17z"></path></svg>
+          </div>
+          <div class="py-3 font-semibold text-xl ">
+            Verified School Profile
+          </div>
+          <div class="text-center">
+            WhatsApp Business API enables Schools with Green Badge upon profile verification that confirms school authenticity.
+          </div>
+        </div>
+    </div>
+    <div class="col-auto group">
+        <div class="size-full p-5 rounded-2xl  [box-shadow:0px_0px_32px_0px_rgba(36,40,46,.12)]  text-center flex flex-col justify-center items-center
+         group-hover:bg-gradient-to-r from-[skyblue]/20 via-[skyblue]/50 to-[skyblue]/20 group-hover:border-transparent duration-300 transition-all 
+         group-hover:scale-95">
+          <div class="*:size-12 sm:*:size-14">
+            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path></svg>
+          </div>
+          <div class="py-3 font-semibold text-xl ">
+            Use Number Of Your Choice
+          </div>
+          <div class="text-center">
+            No restriction on Number, choose the number you want for WhatsApp Business Solution. It can be Mobile, landline, or toll-free.
+          </div>
+        </div>
+    </div>
+    <div class="col-auto group">
+        <div class="size-full p-5 rounded-2xl text-center [box-shadow:0px_0px_32px_0px_rgba(36,40,46,.12)] flex flex-col justify-center items-center
+         group-hover:bg-gradient-to-r from-[skyblue]/20 via-[skyblue]/50 to-[skyblue]/20 group-hover:border-transparent duration-300 transition-all 
+         group-hover:scale-95">
+          <div class="*:size-12 sm:*:size-14">
+            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"></path></svg>
+          </div>
+          <div class="py-3 font-semibold text-xl ">
+            Broadcast To All
+          </div>
+          <div class="text-center">
+            Send Bulk Messages to all Students, Parents, Teachers in Single Click with WhatsApp Business Solutions.
+          </div>
+        </div>
+    </div>
+    <div class="col-auto group">
+        <div class="size-full p-5 rounded-2xl text-center [box-shadow:0px_0px_32px_0px_rgba(36,40,46,.12)]  flex flex-col justify-center items-center
+         group-hover:bg-gradient-to-r from-[skyblue]/20 via-[skyblue]/50 to-[skyblue]/20 group-hover:border-transparent duration-300 transition-all 
+         group-hover:scale-95">
+          <div class="*:size-12 sm:*:size-14">
+            
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 17c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm6-9h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM8.9 6c0-1.71 1.39-3.1 3.1-3.1s3.1 1.39 3.1 3.1v2H8.9V6zM18 20H6V10h12v10z"></path></svg>
+          </div>
+          <div class="py-3 font-semibold text-xl ">
+            Secure Encrypted Chat
+          </div>
+          <div class="text-center">
+            All chat with clients is in encrypted form. WhatsApp ensures completely secure and reliable communication for seamless experience.
+          </div>
+        </div>
+    </div>
+    <div class="col-auto group">
+        <div class="size-full p-5 rounded-2xl text-center [box-shadow:0px_0px_32px_0px_rgba(36,40,46,.12)] flex flex-col justify-center items-center
+         group-hover:bg-gradient-to-r from-[skyblue]/20 via-[skyblue]/50 to-[skyblue]/20 group-hover:border-transparent duration-300 transition-all 
+         group-hover:scale-95">
+          <div class="*:size-12 sm:*:size-14">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="128" cy="256" r="48" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"></circle><circle cx="384" cy="112" r="48" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"></circle><circle cx="384" cy="400" r="48" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"></circle><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="m169.83 279.53 172.34 96.94m0-240.94-172.34 96.94"></path></svg>
+          </div>
+          <div class="py-3 font-semibold text-xl ">
+            Rich Media Sharing
+          </div>
+          <div class="text-center">
+            Take Advantage of WhatsApp's ability to share rich media content like Photos, Video, Location, Documents, Link, etc. with your Users.
+          </div>
+        </div>
+    </div>
+    <div class="col-auto group">
+        <div class="size-full p-5 rounded-2xl  text-center [box-shadow:0px_0px_32px_0px_rgba(36,40,46,.12)] flex flex-col justify-center items-center
+         group-hover:bg-gradient-to-r from-[skyblue]/20 via-[skyblue]/50 to-[skyblue]/20 group-hover:border-transparent duration-300 transition-all 
+         group-hover:scale-95">
+          <div class="*:size-12 sm:*:size-14">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect width="336" height="336" x="128" y="128" fill="none" stroke-linejoin="round" stroke-width="32" rx="57" ry="57"></rect><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="m383.5 128 .5-24a56.16 56.16 0 0 0-56-56H112a64.19 64.19 0 0 0-64 64v216a56.16 56.16 0 0 0 56 56h24"></path></svg>
+          </div>
+          <div class="py-3 font-semibold text-xl ">
+            Easy API Integration
+          </div>
+          <div class="text-center">
+            Edoovi offers Integrated Solutions with school management System with the facility to design personalized ChatBot.
+          </div>
+        </div>
+    </div>
+    </div>
+    </div>
+</section>
+
+
+     <!-- blog end  -->
     <!-- form section start -->
 <!-- component -->
  <section class="relative mb-20">
