53af4fc6f475f492df8ef069cfbc9eb7048fd2f4
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2024-08-05 14:48:16 +0530

    done

--- header/index.php
+++ header/index.php
@@ -143,29 +143,29 @@
                     </div>
                 </div>
                 <div class="sm:w-1/2 w-full h-auto py-3">
-                    <div class="flex m-3 justify-center h-full items-center flex-col hover:translate-y-1 duration-300    bg-gradient-to-r from-[#fdad50] via-[#ffa944] to-[#ff971d]
+                    <div class="flex m-3 justify-center h-full items-center flex-col hover:translate-y-1 duration-300 bg-white
 rounded-xl group shadow-2xl p-3 relative before:absolute before:size-20 before:rounded-full ">
-                        <div class="*:size-12 text-white mb-2">
+                        <div class="*:size-12 text-purple-400  mb-2">
                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
                         </div>
                       
-                        <div class="font-semibold text-white text-xl">
+                        <div class="font-semibold text-purple-400  text-xl">
                         1600+
                         </div>
-                        <div class="text-xl font-semibold text-white">
+                        <div class="text-xl font-semibold text-purple-400 ">
                         Happy Clients
                           </div>
                     </div>
                 </div>
                 <div class="sm:w-1/2 w-full h-auto py-3">
-                    <div class="flex m-3 justify-center h-full items-center flex-col hover:translate-y-1 duration-300 bg-gradient-to-r from-[#fdad50] via-[#ffa944] to-[#ff971d] rounded-xl group shadow-2xl p-3 relative before:absolute before:size-20 before:rounded-full ">
-                        <div class="*:size-12 text-white mb-2 ">
+                    <div class="flex m-3 justify-center h-full items-center flex-col hover:translate-y-1 duration-300 bg-white rounded-xl group shadow-2xl p-3 relative before:absolute before:size-20 before:rounded-full ">
+                        <div class="*:size-12 text-purple-400 mb-2 ">
                         <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path><path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path></svg>
                         </div>  
-                        <div class="font-semibold text-white text-xl">
+                        <div class="font-semibold text-purple-400  text-xl">
                             96%
                         </div>
-                        <div class="text-xl font-semibold text-white">
+                        <div class="text-xl font-semibold text-purple-400 ">
                         Client Retention
                           </div>
                     </div>
@@ -207,12 +207,12 @@ rounded-xl group shadow-2xl p-3 relative before:absolute before:size-20 before:r
      <p class="text-lg font-semibold text-white mt-4 mb-2">Available on the</p>
     <div class="flex gap-4">
     
-    <button class="text-lg w-44  text-white rounded-md px-4 py-2 justify-center font-sembold border-white border  flex">
+    <button class="text-lg lg:w-44  text-white rounded-md px-4 py-2 justify-center font-sembold border-white border  flex">
         Google Play 
         <span class="*:size-6 ms-3">
           <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="m12.954 11.616 2.957-2.957L6.36 3.291c-.633-.342-1.226-.39-1.746-.016l8.34 8.341zm3.461 3.462 3.074-1.729c.6-.336.929-.812.929-1.34 0-.527-.329-1.004-.928-1.34l-2.783-1.563-3.133 3.132 2.841 2.84zM4.1 4.002c-.064.197-.1.417-.1.658v14.705c0 .381.084.709.236.97l8.097-8.098L4.1 4.002zm8.854 8.855L4.902 20.91c.154.059.32.09.495.09.312 0 .637-.092.968-.276l9.255-5.197-2.666-2.67z"></path></svg></span>
       </button>
-      <button class="text-lg w-44  text-white bg-gradient-to-r from-[#fdad50] via-[#ffa944] to-[#ff971d]  rounded-md px-4 py-2 font-sembold  justify-center flex">
+      <button class="text-lg lg:w-44  text-white bg-white  rounded-md px-4 py-2 font-sembold  justify-center flex">
         App Store
         <span class="*:size-6 ms-3">
         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg></span>
@@ -243,16 +243,16 @@ rounded-xl group shadow-2xl p-3 relative before:absolute before:size-20 before:r
                         
                       Comprehensive, Easy To Use Web Based School ERP System Helps You To Move Faster, Save Money, Integrate On-Premises Apps And Data Anytime Anywhere Explore the online school ERP with built-in School App features and start the new education administration journey with the new-edge technology! Along with School App, We offer Management App - bringing the entire school in the palm of your hand, making the whole school management experience effortless.</div>
                         <p class="text-lg font-semibold text-white text-start mt-4 mb-2">Available on the</p>
-    <div class="flex gap-4  flex-wrap">
+    <div class="flex gap-4">
     
-    <button class="text-lg w-44  text-white rounded-md px-4 py-2 justify-center font-sembold border-white border  flex">
+    <button class="sm:text-lg  text-sm lg:w-44  text-white rounded-md px-4 py-2 justify-center font-sembold border-white border  flex">
         Google Play 
-        <span class="*:size-6 ms-3">
+        <span class="*:sm:size-6 *:size-4 sm:ms-3 ms-1">
           <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="m12.954 11.616 2.957-2.957L6.36 3.291c-.633-.342-1.226-.39-1.746-.016l8.34 8.341zm3.461 3.462 3.074-1.729c.6-.336.929-.812.929-1.34 0-.527-.329-1.004-.928-1.34l-2.783-1.563-3.133 3.132 2.841 2.84zM4.1 4.002c-.064.197-.1.417-.1.658v14.705c0 .381.084.709.236.97l8.097-8.098L4.1 4.002zm8.854 8.855L4.902 20.91c.154.059.32.09.495.09.312 0 .637-.092.968-.276l9.255-5.197-2.666-2.67z"></path></svg></span>
       </button>
-      <button class="text-lg w-44  text-white bg-gradient-to-b from-violet-300 to-violet-500  rounded-md px-4 py-2 font-sembold  justify-center flex">
+      <button class="sm:text-lg  text-sm lg:w-44  text-white bg-gradient-to-b from-violet-300 to-violet-500  rounded-md px-4  py-2 font-sembold  justify-center flex">
         App Store
-        <span class="*:size-6 ms-3">
+        <span class="*:sm:size-6 *:size-4 sm:ms-3 ms-1">
         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg></span>
       </button>
     </div>
