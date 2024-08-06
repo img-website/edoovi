e50518e3c6f83b81980edd12d90ddc0f574a1630
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2024-08-06 14:13:14 +0530

    Update index.php

--- blogs/blogInner/index.php
+++ blogs/blogInner/index.php
@@ -177,14 +177,14 @@
                             <div class="grid lg:grid-cols-2 gap-3">
                                 <div class="grid grid-cols-2 lg:grid-cols-1 gap-3">
                                     <figure class="relative w-full h-60">
-                                        <img class="size-full absolute top-0 start-0 object-cover rounded-xl" src="../../assets/img/blogInner01.jpg" alt="Blog Image">
+                                        <img class="size-full absolute top-0 start-0 object-cover rounded-3xl" src="../../assets/img/blogInner01.jpg" alt="Blog Image">
                                     </figure>
                                     <figure class="relative w-full h-60">
-                                        <img class="size-full absolute top-0 start-0 object-cover rounded-xl" src="../../assets/img/blogInner02.jpg" alt="Blog Image">
+                                        <img class="size-full absolute top-0 start-0 object-cover rounded-3xl" src="../../assets/img/blogInner02.jpg" alt="Blog Image">
                                     </figure>
                                 </div>
                                 <figure class="relative w-full h-72 sm:h-96 lg:h-full">
-                                    <img class="size-full absolute top-0 start-0 object-cover rounded-xl" src="../../assets/img/blogInner03.jpg" alt="Blog Image">
+                                    <img class="size-full absolute top-0 start-0 object-cover rounded-3xl" src="../../assets/img/blogInner03.jpg" alt="Blog Image">
                                 </figure>
                             </div>
                         </div>
@@ -208,7 +208,7 @@
                         </blockquote>
 
                         <figure>
-                            <img class="w-full object-cover rounded-xl" src="../../assets/img/blogInner04.jpg" alt="Blog Image">
+                            <img class="w-full object-cover rounded-3xl" src="../../assets/img/blogInner04.jpg" alt="Blog Image">
                         </figure>
 
                         <div class="space-y-3">
@@ -287,7 +287,7 @@
                                         Share
                                     </button>
                                     <div class="hs-dropdown-menu w-56 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden mb-1 z-10 bg-gray-900 shadow-md rounded-xl p-2" role="menu" aria-orientation="vertical" aria-labelledby="hs-blog-article-share-dropdown">
-                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
+                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-xl text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
                                             <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                                 <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
                                                 <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
@@ -295,19 +295,19 @@
                                             Copy link
                                         </a>
                                         <div class="border-t border-gray-600 my-2"></div>
-                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
+                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-xl text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
                                             <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                                 <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
                                             </svg>
                                             Share on Twitter
                                         </a>
-                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
+                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-xl text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
                                             <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                                             </svg>
                                             Share on Facebook
                                         </a>
-                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
+                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-xl text-sm text-gray-400 hover:bg-white/10 focus:outline-none focus:bg-white/10" href="#">
                                             <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                                 <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                                             </svg>
@@ -339,7 +339,7 @@
 
                         <div class="grow">
                             <div class="flex justify-end">
-                                <button type="button" class="py-1.5 px-2.5 inline-flex items-center gap-x-2 text-xs font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                                <button type="button" class="py-1.5 px-2.5 inline-flex items-center gap-x-2 text-xs font-medium rounded-xl border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                                     <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
                                         <circle cx="9" cy="7" r="4" />
@@ -360,8 +360,8 @@
                                 </span>
                             </div>
 
-                            <div class="shrink-0 relative rounded-lg overflow-hidden size-20">
-                                <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="../../assets/img/blog1.png" alt="Blog Image">
+                            <div class="shrink-0 relative rounded-xl overflow-hidden size-20">
+                                <img class="size-full absolute top-0 start-0 object-cover rounded-xl" src="../../assets/img/blog1.png" alt="Blog Image">
                             </div>
                         </a>
 
@@ -372,8 +372,8 @@
                                 </span>
                             </div>
 
-                            <div class="shrink-0 relative rounded-lg overflow-hidden size-20">
-                                <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image">
+                            <div class="shrink-0 relative rounded-xl overflow-hidden size-20">
+                                <img class="size-full absolute top-0 start-0 object-cover rounded-xl" src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image">
                             </div>
                         </a>
 
@@ -384,8 +384,8 @@
                                 </span>
                             </div>
 
-                            <div class="shrink-0 relative rounded-lg overflow-hidden size-20">
-                                <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image">
+                            <div class="shrink-0 relative rounded-xl overflow-hidden size-20">
+                                <img class="size-full absolute top-0 start-0 object-cover rounded-xl" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image">
                             </div>
                         </a>
 
@@ -396,8 +396,8 @@
                                 </span>
                             </div>
 
-                            <div class="shrink-0 relative rounded-lg overflow-hidden size-20">
-                                <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="../../assets/img/blog2.jpg" alt="Blog Image">
+                            <div class="shrink-0 relative rounded-xl overflow-hidden size-20">
+                                <img class="size-full absolute top-0 start-0 object-cover rounded-xl" src="../../assets/img/blog2.jpg" alt="Blog Image">
                             </div>
                         </a>
                     </div>
