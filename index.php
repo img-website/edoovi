d840aa66ea8a6bdf0611046ac7c6e9c5f0c1d88e
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2024-08-07 10:33:05 +0530

    Chnages done in index and client

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                     <li><a href="./blogs" class="[&.active]:text-slate-950 [&.active]:border-b-[2px] [&.active]:border-b-slate-900 inline-flex font-medium text-base active:text-slate-900 text-slate-600">Blogs</a></li>
                     <li><a href="./clients/" class="[&.active]:text-slate-950 [&.active]:border-b-[2px] [&.active]:border-b-slate-900 inline-flex font-medium text-base active:text-slate-900 text-slate-600">Clients</a></li>
                 </ul>
-                    <a href="./contact/" class="bg-gradient-to-b from-violet-300 to-violet-500/90 py-2 px-4 rounded-full shadow-xl shadow-violet-200/95 font-medium text-base text-white duration-300 hover:[box-shadow:0px_0px_10px_0px_#e39943] hover:bg-gradient-to-r hover:from-[#fdad50] hover:via-[#ffa944] hover:to-[#ff971d]">Contant Us</a>
+                <a href="./contact/" class="bg-gradient-to-b from-violet-300 to-violet-500/90 py-2 px-4 rounded-full shadow-xl shadow-violet-200/95 font-medium text-base text-white duration-300 hover:[box-shadow:0px_0px_10px_0px_#e39943] hover:bg-gradient-to-r hover:from-[#fdad50] hover:via-[#ffa944] hover:to-[#ff971d]">Contant Us</a>
             </div>
 
             <input type="checkbox" class="peer/sideToggle hidden" name="sideToggle" id="sideToggle">
@@ -136,7 +136,7 @@
                     <div class="text-gray-500 text-sm mt-3 font-medium leading-normal pe-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident animi veniam cupiditate et nobis, omnis similique distinctio iste maxime enim quo. Quod autem dolorem dolores?</div>
                     <div class="grid grid-cols-3 items-center mt-5">
                         <div>
-                        <button class="bg-gradient-to-b from-violet-300 to-violet-500/90 py-2 px-4 rounded-full shadow-xl shadow-violet-200/95 font-medium text-base text-white duration-300 hover:[box-shadow:0px_0px_10px_0px_#e39943] hover:bg-gradient-to-r hover:from-[#fdad50] hover:via-[#ffa944] hover:to-[#ff971d]">Contant Us</button>
+                            <button class="bg-gradient-to-b from-violet-300 to-violet-500/90 py-2 px-4 rounded-full shadow-xl shadow-violet-200/95 font-medium text-base text-white duration-300 hover:[box-shadow:0px_0px_10px_0px_#e39943] hover:bg-gradient-to-r hover:from-[#fdad50] hover:via-[#ffa944] hover:to-[#ff971d]">Contant Us</button>
                         </div>
                         <div>
                             <div class="text-slate-900 md:text-4xl text-2xl font-semibold">50<span class="text-xl">+</span></div>
@@ -227,7 +227,6 @@
     <!-- hero section end  -->
 
     <!-- career course  -->
-
     <section class="relative lg:py-16 py-10 overflow-hidden">
         <div class="relative container mx-auto max-w-7xl px-6 lg:px-8 z-10 mb-10">
             <p class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-slate-900  font-semibold">
@@ -321,7 +320,6 @@
 
 
     <!-- logo section  -->
-
     <div class="overflow-hidden lg:py-16 py-10">
         <div class="relative container mx-auto max-w-7xl px-6 lg:px-8 z-10 mb-10">
             <p class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-slate-900  font-semibold">
@@ -445,9 +443,7 @@
             </div>
         </div>
     </div>
-
     <!-- logo section end  -->
-
 
     <!-- seamless solution for  -->
     <section class="relative lg:py-16 py-10 overflow-hidden">
@@ -587,7 +583,6 @@
     </section>
     <!-- end  -->
 
-
     <!-- testimonial section start -->
     <div class="overflow-hidden">
         <div class="relative max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
@@ -603,13 +598,6 @@
                 </div>
             </div>
 
-            <div class="absolute bottom-0 end-0 transform lg:translate-x-32" aria-hidden="true">
-                <svg class="w-40 h-auto sm:w-72" width="1115" height="636" viewBox="0 0 1115 636" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.990203 279.321C-1.11035 287.334 3.68307 295.534 11.6966 297.634L142.285 331.865C150.298 333.965 158.497 329.172 160.598 321.158C162.699 313.145 157.905 304.946 149.892 302.845L33.8132 272.418L64.2403 156.339C66.3409 148.326 61.5475 140.127 53.5339 138.026C45.5204 135.926 37.3213 140.719 35.2207 148.733L0.990203 279.321ZM424.31 252.289C431.581 256.26 440.694 253.585 444.664 246.314C448.635 239.044 445.961 229.931 438.69 225.96L424.31 252.289ZM23.0706 296.074C72.7581 267.025 123.056 230.059 187.043 212.864C249.583 196.057 325.63 198.393 424.31 252.289L438.69 225.96C333.77 168.656 249.817 164.929 179.257 183.892C110.144 202.465 54.2419 243.099 7.92943 270.175L23.0706 296.074Z" fill="currentColor" class="fill-orange-500" />
-                    <path d="M451.609 382.417C446.219 388.708 446.95 398.178 453.241 403.567L555.763 491.398C562.054 496.788 571.524 496.057 576.913 489.766C582.303 483.474 581.572 474.005 575.281 468.615L484.15 390.544L562.222 299.413C567.612 293.122 566.881 283.652 560.59 278.263C554.299 272.873 544.829 273.604 539.44 279.895L451.609 382.417ZM837.202 559.655C841.706 566.608 850.994 568.593 857.947 564.09C864.9 559.586 866.885 550.298 862.381 543.345L837.202 559.655ZM464.154 407.131C508.387 403.718 570.802 395.25 638.136 410.928C704.591 426.401 776.318 465.66 837.202 559.655L862.381 543.345C797.144 442.631 718.724 398.89 644.939 381.709C572.033 364.734 504.114 373.958 461.846 377.22L464.154 407.131Z" fill="currentColor" class="fill-cyan-500" />
-                    <path d="M447.448 0.194357C439.203 -0.605554 431.87 5.43034 431.07 13.6759L418.035 148.045C417.235 156.291 423.271 163.623 431.516 164.423C439.762 165.223 447.095 159.187 447.895 150.942L459.482 31.5025L578.921 43.0895C587.166 43.8894 594.499 37.8535 595.299 29.6079C596.099 21.3624 590.063 14.0296 581.818 13.2297L447.448 0.194357ZM1086.03 431.727C1089.68 439.166 1098.66 442.239 1106.1 438.593C1113.54 434.946 1116.62 425.96 1112.97 418.521L1086.03 431.727ZM434.419 24.6572C449.463 42.934 474.586 81.0463 521.375 116.908C568.556 153.07 637.546 187.063 742.018 200.993L745.982 171.256C646.454 157.985 582.444 125.917 539.625 93.0974C496.414 59.978 474.537 26.1903 457.581 5.59138L434.419 24.6572ZM742.018 200.993C939.862 227.372 1054.15 366.703 1086.03 431.727L1112.97 418.521C1077.85 346.879 956.138 199.277 745.982 171.256L742.018 200.993Z" fill="currentColor" class="fill-red" />
-                </svg>
-            </div>
 
             <div class="swiper testimonailSwiper">
                 <div class="swiper-wrapper">
@@ -814,13 +802,13 @@
                     <div class="relative z-50 text-white py-6 rounded-xl ">
                         <p class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-slate-900  font-semibold">
                             <span class="font-semibold">Empowering</span>
-    
+
                             <span class="font-bold text-slate-900">Institutions</span>
                         </p>
                         <p class="text-base text-slate-800 mt-4">We aim at contributing to the educational system with best-in-class edTech solutions that create maximum value for Institutions.</p>
                         <button class="h-max mt-4 bg-gradient-to-b  from-violet-300 to-violet-500/90 py-2 px-4 rounded-full shadow-xl shadow-violet-200/10 font-medium text-base text-white">Request Demo</button>
                     </div>
-    
+
                 </div>
                 <div class="lg:w-1/2 w-full relative">
                     <div class="flex  flex-wrap relative item-stretch">
@@ -832,7 +820,7 @@
                                         <path d="M9.68 13.69 12 11.93l2.31 1.76-.88-2.85L15.75 9h-2.84L12 6.19 11.09 9H8.25l2.31 1.84-.88 2.85zM20 10c0-4.42-3.58-8-8-8s-8 3.58-8 8c0 2.03.76 3.87 2 5.28V23l6-2 6 2v-7.72A7.96 7.96 0 0 0 20 10zm-8-6c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6 2.69-6 6-6zm0 15-4 1.02v-3.1c1.18.68 2.54 1.08 4 1.08s2.82-.4 4-1.08v3.1L12 19z"></path>
                                     </svg>
                                 </div>
-    
+
                                 <div class="font-semibold sm:text-xl text-lg text-white">
                                     22+
                                 </div>
@@ -852,7 +840,7 @@
                                         <line x1="23" y1="11" x2="17" y2="11"></line>
                                     </svg>
                                 </div>
-    
+
                                 <div class="sm:text-xl text-sm  md:font-semibold font-medium text-center ">
                                     1600+
                                 </div>
@@ -888,7 +876,7 @@
                                         <path d="m12 15 1.57-3.43L17 10l-3.43-1.57L12 5l-1.57 3.43L7 10l3.43 1.57z"></path>
                                     </svg>
                                 </div>
-    
+
                                 <div class="sm:text-xl text-sm  md:font-semibold font-medium text-center text-white">
                                     2500+
                                 </div>
@@ -897,11 +885,11 @@
                                 </div>
                             </div>
                         </div>
-    
-    
+
+
                     </div>
                 </div>
-    
+
             </div>
         </div>
         </div>
@@ -922,7 +910,7 @@
                                     <span class="font-semibold">
                                         The Ultimate School ERP With Mobile App For Modern School System</span>
 
-                                  
+
                                 </p>
                             </div>
                         </div>
@@ -960,8 +948,6 @@
         </div>
 
     </section>
-
-
     <!-- cta end  -->
 
     <!-- key benifits section start -->
@@ -975,7 +961,7 @@
                 <span class="font-semibold text-slate-900">Lots Of Time & Making Best Of Resources.</span>
             </p>
             <div class="md:text-base text-sm text-black/70 mt-2 font-semibold text-balance">
-               Edoovi, the comprehensive school management system software meets all your school management requirements. It is best in class software covers all academic & administrative management functionalities that require to drive an educational institute. Starting from admissions to regular academic activities, exam management, fees collection, employee payroll, Edoovi school management system can relieve the staff and make the process hassle-free!
+                Edoovi, the comprehensive school management system software meets all your school management requirements. It is best in class software covers all academic & administrative management functionalities that require to drive an educational institute. Starting from admissions to regular academic activities, exam management, fees collection, employee payroll, Edoovi school management system can relieve the staff and make the process hassle-free!
             </div>
         </div>
         <div class="container mx-auto max-w-7xl px-6 lg:px-8 py-10 sm:px-6 ">
@@ -1081,9 +1067,8 @@
 
 
 
-    
-    <!-- blog section  -->
 
+    <!-- blog section  -->
     <section class="container px-6 lg:px-8 mx-auto max-w-7xl lg:pb-16 pb-10 overflow-hidden">
         <div class="relative z-10 mb-8 md:mb-12">
             <p class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-slate-900  font-semibold">
@@ -1337,17 +1322,13 @@
             </div>
         </div>
     </section>
-
     <!-- blog end  -->
 
     <!-- form section start -->
     <section class="relative mb-20 overflow-hidden ">
         <div class="absolute  top-0 !right-0 [background:radial-gradient(circle,_rgba(255,231,_186,_0.7),rgba(255,200,_200,_0.5))] blur-2xl size-72"></div>
-
         <div class="flex justify-center items-center mx-auto max-w-7xl lg:px-8 px-6  relative z-10">
-            <!-- COMPONENT CODE -->
             <div class="">
-
                 <div class="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-64 mr-auto rounded-2xl shadow-2xl">
                     <div class="flex">
                         <h1 class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-slate-900  font-semibold">Send us <br /> a message</h1>
@@ -1462,15 +1443,7 @@
             </div>
         </div>
     </section>
-
     <!-- form section end  -->
-
-
-
-
-
-    <!-- footer start        -->
-    
 
     <!-- footer -->
     <section class="py-10 bg-slate-800 sm:pt-16 lg:pt-24 relative">
@@ -1728,8 +1701,6 @@
     });
 </script>
 <!-- end  -->
-
-
 
 
 
